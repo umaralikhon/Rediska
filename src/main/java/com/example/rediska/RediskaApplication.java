5b71bfe3704b512ebecd1be9f0b77ced6b9d02ea
Umaralikhon <umaralikhon@gmail.com>
2024-03-27 01:40:20 +0500

    Initial commit

---
+++ src/main/java/com/example/rediska/RediskaApplication.java
@@ -0,0 +1,13 @@
+package com.example.rediska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RediskaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RediskaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rediska/RediskaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rediska;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RediskaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
